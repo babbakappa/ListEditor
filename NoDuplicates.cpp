a11ca4efeff5a0f79e2ba274a33268ea4f829441
babbakappa <hohrsibi81@gmail.com>
2026-06-17 11:51:04 +0700

    Небольшие изменения

--- NoDuplicates.cpp
+++ NoDuplicates.cpp
@@ -2,45 +2,6 @@
 
 using namespace std;
 
-void NoDuplicates::ExtractData(string filepath)
-{
-	ifstream fin(filepath);
-	if (fin.is_open() == false) {
-		cout << "Ошибка! Файла не существует!\n";
-		system("pause");
-		return;
-	}
-	string temporary;
-	while (getline(fin, temporary)) {
-		array.push_back(temporary);
-	}
-	fin.close();
-}
-
-void NoDuplicates::SaveData(std::string filepath)
-{
-	ofstream fout(filepath);
-	for (int i = 0; i < array.size(); i++) {
-		fout << array[i] << "\n";
-	}
-}
-
-void NoDuplicates::InputData(int size)
-{
-	for (int i = 0; i < size; i++) {
-		string element;
-		getline(cin, element);
-		array.push_back(element);
-	}
-}
-
-void NoDuplicates::OutputData()
-{
-	for (int i = 0; i < array.size(); i++) {
-		cout << array[i] << "\n";
-	}
-}
-
 void NoDuplicates::DeleteDuplicates()
 {
 	unordered_set<string> seen;
@@ -50,4 +11,3 @@ void NoDuplicates::DeleteDuplicates()
 		});
 	array.erase(new_end, array.end());
 }
-
--- NoDuplicates.hpp
+++ NoDuplicates.hpp
@@ -1,20 +1,10 @@
 #ifndef NODUPLICATES_HPP
 #define NODUPLICATES_HPP
-#include <vector>
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <algorithm>
+#include "Methods.hpp"
 #include <unordered_set>
 
-class NoDuplicates {
-private:
-	std::vector<std::string> array;
+class NoDuplicates : public Methods<std::string> {
 public:
-	void ExtractData(std::string filepath);
-	void SaveData(std::string filepath);
-	void InputData(int size);
-	void OutputData();
 	void DeleteDuplicates();
 };
 
