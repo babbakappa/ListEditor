04a8837cd935a863ea2d8a645f8ea8725b917c37
babbakappa <hohrsibi81@gmail.com>
2026-07-13 14:18:35 +0700

    Новый метод: инверсирование

---
+++ Inverse.cpp
@@ -0,0 +1,8 @@
+#include "Inverse.hpp"
+
+using namespace std;
+
+void Inverse::InverseList()
+{
+    reverse(array.begin(), array.end());
+}
---
+++ Inverse.hpp
@@ -0,0 +1,11 @@
+#ifndef INVERSE_HPP
+#define INVERSE_HPP
+#include "Methods.hpp"
+#include <algorithm>
+
+class Inverse : public Methods<string> {
+public:
+	void InverseList();
+};
+
+#endif
